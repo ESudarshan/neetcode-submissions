aca0b30d72975e0817ebf2564bcd991dc32f8c37
ESudarshan <sudarshanekambe@gmail.com>
2026-05-05 23:47:17 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String minWindow(String s, String t) {
+
+        Map<Character, Integer> tMap = new HashMap<>();
+        for(char tCh : t.toCharArray()) {
+            tMap.put(tCh, tMap.getOrDefault(tCh, 0) + 1);
+        }
+
+        int ansLen = s.length() + 1;
+        String ansStr = "";
+        int n = s.length();
+        for(int i=0; i<n; i++) {
+            Map<Character, Integer> sMap = new HashMap<>();
+            for(int j=i; j<n; j++) {
+                sMap.put(s.charAt(j), sMap.getOrDefault(s.charAt(j), 0) + 1);
+                boolean valid = true;
+                for(char ch : tMap.keySet()) {
+                    if(!sMap.containsKey(ch) || tMap.get(ch) > sMap.get(ch)) {
+                        valid = false;
+                        break;
+                    }
+                }
+                if(valid) {
+                    if(j-i+1 < ansLen) {
+                        ansLen = j-i+1;   
+                        ansStr = s.substring(i, j+1);
+                    }
+
+                }
+            }
+        }
+        return ansStr;
+    }
+}

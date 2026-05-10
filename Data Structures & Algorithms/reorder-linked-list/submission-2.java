d4ca7178d7832b07079a3c7aad969ee53e527a48
ESudarshan <sudarshanekambe@gmail.com>
2026-05-10 20:34:40 +0530

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public void reorderList(ListNode head) {
+        Map<Integer, ListNode> map = new HashMap<>();
+        int index = 0;
+        ListNode curr = head;
+        while(curr != null) {
+            map.put(index, curr);
+            index++;
+            curr = curr.next;
+        }
+        int n = index;
+
+        curr = head;
+        for(int i=1; i<n; i++) {
+            int idx = i/2;
+            if(i % 2 == 1) {
+                idx = n - (idx + 1);
+            } 
+            curr.next = map.get(idx);
+            curr = curr.next;
+            curr.next = null;
+        }
+        
+    }
+}

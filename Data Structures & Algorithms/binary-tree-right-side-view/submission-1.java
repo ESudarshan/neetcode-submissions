9e62a5f25aa2ba5b61a4efaadcdb58fba40196a6
ESudarshan <sudarshanekambe@gmail.com>
2026-05-17 16:09:05 +0530

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) {
+            return list;
+        }
+        dfs(root, 0, list);
+        return list;
+/*
+        List<Integer> list = new ArrayList<>();
+        if(root == null) {
+            return list;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int n = q.size();
+            for(int i=0; i<n; i++) {
+                TreeNode t = q.poll();
+                if(t.left != null) {
+                    q.offer(t.left);
+                }
+                if(t.right != null) {
+                    q.offer(t.right);
+                }
+                if(i == n - 1) {
+                    list.add(t.val);
+                } 
+            }
+        }
+        return list;
+*/        
+    }
+
+    public void dfs(TreeNode root, int l, List<Integer> list) {
+        if(root == null) {
+            return;
+        }
+        if(list.size() == l) {
+            list.add(root.val);
+        }
+        dfs(root.right, l+1, list);
+        dfs(root.left, l+1, list);
+    }
+
+}

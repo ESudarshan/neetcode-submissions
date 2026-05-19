95c917d4d0d2342cfa9fbb8356b7081ccb77d7e5
ESudarshan <sudarshanekambe@gmail.com>
2026-05-20 00:57:37 +0530

    Add: design-twitter-feed - submission-24

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-24.java
@@ -0,0 +1,65 @@
+class Twitter {
+
+    Map<Integer, List<Pair<Integer, Integer>>> userIdTweetIdMap;
+    Map<Integer, Set<Integer>> userIdFolloweeIdMap;
+    int timestamp;
+
+    public Twitter() {
+        userIdTweetIdMap = new HashMap<>();
+        userIdFolloweeIdMap = new HashMap<>();
+        timestamp = 0;
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        timestamp++;
+        userIdTweetIdMap.putIfAbsent(userId, new ArrayList<>());
+        userIdTweetIdMap.get(userId).add(new Pair(timestamp, tweetId));
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        List<Pair<Integer, Integer>> tweets = new ArrayList(userIdTweetIdMap.getOrDefault(userId, new ArrayList<>()));
+        for(int followeeId : userIdFolloweeIdMap.getOrDefault(userId, new HashSet<>())) {
+            tweets.addAll(userIdTweetIdMap.get(followeeId));
+        }
+        return getKMostRecent(tweets, 10);
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        if(followerId != followeeId) {
+            userIdFolloweeIdMap.putIfAbsent(followerId, new HashSet<>());
+            userIdFolloweeIdMap.get(followerId).add(followeeId);
+        }
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        if(followerId != followeeId) {
+            userIdFolloweeIdMap.get(followerId).remove(followeeId);
+        }
+    }
+
+    List<Integer> getKMostRecent(List<Pair<Integer, Integer>> tweets, int k) {
+
+        Collections.sort(tweets, (a, b) -> b.getKey() - a.getKey());
+        tweets = tweets.size() > k ? tweets.subList(0, k) : tweets;
+        return tweets.stream().map(Pair::getValue).distinct().collect(Collectors.toList());
+
+/*
+        PriorityQueue<Pair<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> a.getKey() - b.getKey());
+        for(Pair<Integer, Integer> tweet : tweets) {
+            minHeap.offer(tweet);
+            if(minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        List<Integer> mr = new ArrayList<>();
+        while(!minHeap.isEmpty()) {
+            int tweetId = minHeap.poll().getValue();
+            if(!mr.contains(tweetId)) {
+                mr.add(0, tweetId);
+            }
+        }
+        return mr;
+        */
+    }
+
+}

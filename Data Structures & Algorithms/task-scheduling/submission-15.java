cd9a8ae40a560b529adc8d5d8c5b550c2edfadc4
ESudarshan <sudarshanekambe@gmail.com>
2026-05-19 22:39:26 +0530

    Add: task-scheduling - submission-15

---
+++ Data Structures & Algorithms/task-scheduling/submission-15.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+
+        Map<Character, Integer> fMap =new HashMap<>();
+        for(char task : tasks) {
+            fMap.put(task, fMap.getOrDefault(task, 0) + 1);
+        }
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for(char task : fMap.keySet()) {
+            maxHeap.offer(fMap.get(task));
+        }
+
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        int pt = 0;
+        while(!maxHeap.isEmpty() || !q.isEmpty()) {
+            if(maxHeap.isEmpty()) {
+                pt = q.element().getValue();
+            } else if(!maxHeap.isEmpty()) {
+                pt++;
+                int f = maxHeap.poll();
+                // System.out.print("-["+ pt + "|" + t + "|" + f +"]-");
+                f--; 
+                if(f != 0) {
+                    Pair pair = new Pair(f, pt + n);
+                    q.add(pair);
+                }
+            }
+            
+            if(!q.isEmpty() && pt == q.element().getValue()) {
+                Pair<Integer, Integer> pair = q.remove();
+                maxHeap.offer(pair.getKey());
+            }
+
+        }
+
+        return pt;
+    }
+}

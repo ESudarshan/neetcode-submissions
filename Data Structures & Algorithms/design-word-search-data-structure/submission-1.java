ff5f4286f072447c6462286fd09119c2f1796594
ESudarshan <sudarshanekambe@gmail.com>
2026-05-20 22:19:53 +0530

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.java
@@ -0,0 +1,53 @@
+class WordDictionary {
+
+    Node root;
+
+    public WordDictionary() {
+        root = new Node();
+    }
+
+    public void addWord(String word) {
+        Node curr = root;
+        for(char ch : word.toCharArray()) {
+            if(!curr.children.containsKey(ch)) {
+                curr.children.put(ch, new Node());
+            }
+            curr = curr.children.get(ch);
+        }
+        curr.eow = true;
+    }
+
+    public boolean search(String word) {
+        return search(root, word);
+    }
+
+    public boolean search(Node root, String word) {
+        Node curr = root;
+        for(char ch : word.toCharArray()) {
+            if(ch == '.') {
+                for(Node node : curr.children.values()) {
+                    if(search(node, word.substring(word.indexOf(ch) + 1))) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            if(!curr.children.containsKey(ch)) {
+                return false;
+            }
+            curr = curr.children.get(ch);
+        }
+        return curr.eow;
+    }
+
+}
+
+class Node {
+    Map<Character, Node> children;
+    boolean eow;
+
+    Node() {
+        children = new HashMap<>();
+        eow = false;
+    }
+}

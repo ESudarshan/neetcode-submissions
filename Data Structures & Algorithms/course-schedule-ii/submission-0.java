da9cbe56751c5b368fbc05bbebb1b59f7babe81f
ESudarshan <sudarshanekambe@gmail.com>
2026-05-27 00:05:41 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] output = new int[numCourses];
+        
+        int[] indeg = new int[numCourses];
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int[] prereq : prerequisites) {
+            graph.computeIfAbsent(prereq[1], k -> new ArrayList<>()).add(prereq[0]);
+            indeg[prereq[0]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<numCourses; i++) {
+            if(indeg[i] == 0) {
+                q.add(i);
+            }
+        }
+        
+        int k = 0;
+        while(!q.isEmpty()) {
+            int course = q.poll();
+            System.out.println(course);
+            output[k] = course;
+            k++;
+            for(int dependent : graph.getOrDefault(course, new ArrayList<>())) {
+                indeg[dependent]--;
+                if(indeg[dependent] == 0) {
+                    q.add(dependent);
+                }
+            }
+        }
+
+        return k == numCourses ? output : new int[0];
+    }
+}

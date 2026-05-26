e471bc183b01f8a9af1c4e58a6d7c2ab67c5897f
ESudarshan <sudarshanekambe@gmail.com>
2026-05-26 16:40:04 +0530

    Add: word-ladder - submission-1

---
+++ Data Structures & Algorithms/word-ladder/submission-1.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>();
+        wordSet.add(beginWord);
+        for(String word : wordList) {
+            wordSet.add(word);
+        }
+        // wordSet.add(endWord);
+        
+        Map<String, List<String>> wordPatternMap = new HashMap<>();
+        Map<String, List<String>> patternWordMap = new HashMap<>();
+        
+        for(String word : wordSet) {
+            List<String> patterns = new ArrayList<>();
+            for(int i=0; i<word.length(); i++) {
+                StringBuilder patternBuilder = new StringBuilder(word);
+                patternBuilder.setCharAt(i, '*');
+                String pattern = patternBuilder.toString();
+                patterns.add(pattern);
+                patternWordMap.putIfAbsent(pattern, new ArrayList<>());
+                patternWordMap.get(pattern).add(word);
+            }
+            wordPatternMap.put(word, patterns);
+        }
+
+        Queue<String> q = new LinkedList<>();
+        q.add(beginWord);
+        wordSet.remove(beginWord);
+        int l = 1;
+        while(!q.isEmpty()) {
+            int n = q.size();
+            for(int i=0; i<n; i++) {
+                String frontWord  = q.remove();
+                if(frontWord.equals(endWord)) {
+                    System.out.println(frontWord);
+                    return l;
+                }
+                for(String pattern : wordPatternMap.get(frontWord)) {
+                    for(String word : patternWordMap.get(pattern)) {
+                        if(wordSet.contains(word)) {
+                            q.add(word);
+                            wordSet.remove(word);
+                        }
+                    }    
+                }
+            }
+            l++;
+        }
+        return 0;
+    }
+}

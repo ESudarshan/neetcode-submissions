540d062468b14b9dfaca0a51c6e30253db0f2092
ESudarshan <sudarshanekambe@gmail.com>
2026-05-27 23:55:50 +0530

    Add: climbing-stairs - submission-11

---
+++ Data Structures & Algorithms/climbing-stairs/submission-11.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    int[] mem;
+
+    public int climbStairs(int n) {
+        mem = new int[n + 1];
+        Arrays.fill(mem, -1);
+        return climb(0, n);
+    }
+
+    public int climb(int c, int n) {
+        if(c > n) {
+            return 0;
+        }
+        if(c == n) {
+            return 1;
+        }
+        if(mem[c] != -1) {
+            return mem[c];
+        }
+        mem[c] = climb(c + 1, n) + climb(c + 2, n);
+        return mem[c];
+    }
+}

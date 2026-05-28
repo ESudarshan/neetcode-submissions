241130c693f4b21587cb6b655efb1469354a3a1e
ESudarshan <sudarshanekambe@gmail.com>
2026-05-28 22:36:37 +0530

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    int[] mem;
+
+    public int numDecodings(String s) {
+        int n = s.length();
+        mem = new int[n];
+        Arrays.fill(mem, -1);
+        return decode(s, 0, n);
+    }
+
+    public int decode(String s, int i, int n) {
+        if(i == n) {
+            return 1;
+        }
+        if(i > n) {
+            return 0;
+        }
+        if(mem[i] != -1) {
+            return mem[i];
+        }
+        if(s.charAt(i) == '0') {
+            return 0;
+        }
+        int one = decode(s, i + 1, n);
+        int two = 0;
+        if((i + 2) <= n && Integer.parseInt(s.substring(i, i + 2)) <= 26) {
+            two = decode(s, i + 2, n);
+        }
+        mem[i] = one + two;
+        return mem[i];
+    }
+}

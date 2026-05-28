20282d28808f5aa2cef968f401d83732a48d71e4
ESudarshan <sudarshanekambe@gmail.com>
2026-05-28 13:27:20 +0530

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    int[] mem;
+    
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        mem = new int[n+1];
+        Arrays.fill(mem, -1);
+        int minCostAt0 = minCost(cost, 0, n);
+        Arrays.fill(mem, -1);
+        int minCostAt1 = minCost(cost, 1, n);
+        System.out.println(minCostAt0);
+        System.out.println(minCostAt1);
+        return Math.min(minCostAt0, minCostAt1);
+    }
+
+    public int minCost(int[] cost, int floor, int n) {
+        if(floor > n) {
+            return Integer.MAX_VALUE;
+        }
+        if(floor == n) {
+            return 0;
+        }
+        if(mem[floor] != -1) {
+            return mem[floor];
+        }
+        mem[floor] = cost[floor] + Math.min(minCost(cost, floor + 1, n), minCost(cost, floor + 2, n));
+        return mem[floor];    
+    }
+}

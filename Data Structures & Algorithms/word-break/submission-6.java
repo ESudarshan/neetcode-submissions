41beb2cd618e62c310753c2dce87c5e33699ab95
ESudarshan <sudarshanekambe@gmail.com>
2026-05-30 21:09:40 +0530

    Add: word-break - submission-6

---
+++ Data Structures & Algorithms/word-break/submission-6.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    int[][] mem;
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        mem = new int[n][n];
+        for(int i=0; i<n; i++) {
+            Arrays.fill(mem[i], -1);
+        }
+        return breakWord(s, 0, 0, n, wordDict);
+    }
+
+    public boolean breakWord(String s, int start, int end, int n, List<String> wordDict) {
+        if(start == n) {
+            return true;
+        }
+
+        if(end == n) {
+            return false;
+        }
+
+        if(mem[start][end] != -1) {
+            return mem[start][end] == 0 ? false : true;
+        }
+
+        boolean res = false;
+        if(wordDict.contains(s.substring(start, end + 1))) {
+            res = breakWord(s, end + 1, end + 1, n, wordDict);
+        }
+        res = breakWord(s, start, end + 1, n, wordDict) || res;
+        mem[start][end] = res == false ? 0 : 1;
+        return res;
+    }
+
+}

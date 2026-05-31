3e88c6c0cbd2b0c5c67e4f3abb5332a07f28f436
ESudarshan <sudarshanekambe@gmail.com>
2026-06-01 00:04:42 +0530

    Add: longest-increasing-subsequence - submission-31

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-31.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    int[][] mem;
+    
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        mem = new int[n][n];
+        for(int[] m : mem) {
+            Arrays.fill(m, -1);
+        }
+        return lis(nums, 0, -1, n);
+    }
+
+    public int lis(int[] nums, int i, int j, int n) {
+        if(i == n) {
+            return 0;
+        }
+        if(j != -1 && mem[i][j] != -1) {
+            return mem[i][j];
+        }
+        int longest = 0;
+        if(j == -1 || nums[j] < nums[i]) {
+            longest = 1 + lis(nums, i + 1, i, n); 
+        }
+        longest = Math.max(longest, lis(nums, i + 1, j, n));
+        if(j != -1) {
+            mem[i][j] = longest;
+        }
+        return longest;
+    }
+
+}

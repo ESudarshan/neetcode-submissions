02928f5ee476b0e5a3b2e66ecb1b75af22c7b9d7
ESudarshan <sudarshanekambe@gmail.com>
2026-06-02 00:22:55 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        return canJump(nums, 0, n);
+    }
+
+    public boolean canJump(int[] nums, int i, int n) {
+        if(i == n - 1) {
+            return true;
+        }
+
+        for(int k = 1; k <= nums[i] ; k++) {
+            if(canJump(nums, i + k, n)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}

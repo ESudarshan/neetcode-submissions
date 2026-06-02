bb15b6715b1a77af149b3da118106c264427349c
ESudarshan <sudarshanekambe@gmail.com>
2026-06-02 22:16:16 +0530

    Add: valid-parenthesis-string - submission-15

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-15.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int n = s.length();
+        int minLeft = 0;
+        int maxLeft = 0;
+        for(int i=0; i<n; i++) {
+            if(s.charAt(i) == '(') {
+                minLeft++;
+                maxLeft++;
+            }
+
+            if(s.charAt(i) == ')') {
+                minLeft--;
+                maxLeft--;
+            }
+
+            if(s.charAt(i) == '*') {
+                minLeft--;
+                maxLeft++;
+            }
+
+            if(maxLeft < 0) {
+                return false;
+            }
+
+            if(minLeft < 0) {
+                minLeft = 0;
+            }
+        }
+        
+        return minLeft == 0;
+    }
+}

a99fa7253e67a580101bbfb3feb91832595740f1
ESudarshan <sudarshanekambe@gmail.com>
2026-06-03 14:07:12 +0530

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        int n = hand.length;
+
+        if(n % groupSize != 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            map.put(hand[i], map.getOrDefault(hand[i], 0) + 1);
+        }
+
+        Arrays.sort(hand);
+        for(int i=0; i<n; i++) {
+            if(!map.containsKey(hand[i])) {
+               continue; 
+            }
+            for(int k=0; k<groupSize; k++) {
+                int key = hand[i] + k; 
+                if(!map.containsKey(key)) {
+                    return false;
+                }
+                if(map.get(key) == 1) {
+                    map.remove(key);
+                } else {
+                    map.put(key, map.get(key) - 1);
+                }
+            }
+        }
+
+        return true;
+    }
+}

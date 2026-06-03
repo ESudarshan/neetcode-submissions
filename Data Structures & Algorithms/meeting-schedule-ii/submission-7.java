7d31065497d53065f259bdeb2c35654e497af386
ESudarshan <sudarshanekambe@gmail.com>
2026-06-04 00:50:28 +0530

    Add: meeting-schedule-ii - submission-7

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-7.java
@@ -0,0 +1,35 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        int minStart = Integer.MAX_VALUE;
+        int maxEnd = Integer.MIN_VALUE;
+        for(Interval interval : intervals) {
+            minStart = Math.min(minStart, interval.start);
+            maxEnd = Math.max(maxEnd, interval.end);
+        }
+        int minRooms = 0;
+        for(int i=minStart; i<=maxEnd; i++) {
+            int conflicts = 0;
+            for(Interval interval : intervals) {
+                if(i>=interval.start && i<interval.end) {
+                    conflicts++;
+                }
+            }
+            minRooms = Math.max(minRooms, conflicts);
+            if(minRooms == intervals.size()) {
+                break;
+            }
+        }
+        return minRooms;
+    }
+}

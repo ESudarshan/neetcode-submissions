ae5deb8a938e5c8293bde8099e6ab6d39cbb3bed
ESudarshan <sudarshanekambe@gmail.com>
2026-06-06 23:52:59 +0530

    Add: network-delay-time - submission-14

---
+++ Data Structures & Algorithms/network-delay-time/submission-14.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int[] delay = new int[n+1];
+        Arrays.fill(delay, Integer.MAX_VALUE);
+
+        Map<Integer, List<int[]>> adjl = new HashMap<>();
+        for(int[] time : times) {
+            adjl.putIfAbsent(time[0], new ArrayList<>());
+            adjl.get(time[0]).add(new int[] {time[1], time[2]}); 
+        }
+        
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        minHeap.offer(new int[] {k, 0});
+
+        Set<Integer> visited = new HashSet<>();
+
+        while(!minHeap.isEmpty()) {
+            int[] pair = minHeap.poll();
+            int u = pair[0];
+            int uw = pair[1];
+
+            if(visited.contains(u)) {
+                continue;
+            }
+            visited.add(u);
+
+            delay[u] = uw;
+
+            if(adjl.containsKey(u)) {
+                for(int[] uvpair : adjl.get(u)) {
+                    int v = uvpair[0];
+                    int uvw = uvpair[1];
+                    if(!visited.contains(v)) {
+                        minHeap.offer(new int[] {v, uw + uvw});
+                    }
+                }
+            }
+        }
+
+        int maxDelay = -1;
+        for(int i=1; i<=n; i++) {
+            maxDelay = Math.max(maxDelay, delay[i]);
+        }
+
+        return maxDelay == Integer.MAX_VALUE ? -1 : maxDelay;
+    }
+}

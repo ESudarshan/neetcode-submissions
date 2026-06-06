cbe1072f262c580a4ec7e5a9c54e44d5389b45cf
ESudarshan <sudarshanekambe@gmail.com>
2026-06-06 13:45:51 +0530

    Add: coin-change-ii - submission-15

---
+++ Data Structures & Algorithms/coin-change-ii/submission-15.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    /*
+        public int change(int[] coins, int i, int n, int amount) {
+        if(i == n) {
+            if(amount == 0) {
+                return 1;
+            }
+            return 0;
+        }
+        if(amount < 0 ) {
+            return 0;
+        }
+        if(mem[i][amount] == null) {
+            mem[i][amount] = change(coins, i, n, amount-coins[i]) + change(coins, i+1, n, amount);
+        }
+        return mem[i][amount];
+    }
+   */ 
+    
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        Arrays.sort(coins);
+
+        int[][] dp = new int[n+1][amount+1];
+        for(int i=0; i<=n; i++) {
+            dp[i][0] = 1;
+        }
+        
+        for(int i=n-1; i>=0; i--) {
+            for(int j=1; j<=amount; j++) {
+                if(j >= coins[i]) {
+                    dp[i][j] = dp[i][j-coins[i]] + dp[i+1][j];
+                }
+            }
+        }
+
+        return dp[0][amount];
+    }
+}

4ce4043b4d49b4b9875612768aa94e70a31c23a1
ESudarshan <sudarshanekambe@gmail.com>
2026-06-07 17:51:56 +0530

    Add: min-cost-to-connect-points - submission-9

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-9.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        int v = points.length;
+
+        int[] weight = new int[v];
+        Arrays.fill(weight, Integer.MAX_VALUE);
+
+        boolean[] visited = new boolean[v];
+        int visits = 1;
+        int res = 0;
+
+        weight[0] = 0;
+        while(visits <= v) {
+            int currVertex = -1;
+            int currWeight = Integer.MAX_VALUE;
+
+            for(int i=0; i<v; i++) {
+                if(!visited[i]) {
+                    if(weight[i] < currWeight) {
+                        currWeight = weight[i];
+                        currVertex = i;
+                    }
+                }
+            }
+
+            visited[currVertex] = true;
+            visits++;
+            res += currWeight;
+
+            for(int i=0; i<v; i++) {
+                if(!visited[i]) {
+                    int md = Math.abs(points[currVertex][0] - points[i][0]) + Math.abs(points[currVertex][1] - points[i][1]);
+                    if(md < weight[i]) {
+                        weight[i] = md;
+                    }                    
+                }
+            }
+
+        }
+
+        return res;
+    }
+}

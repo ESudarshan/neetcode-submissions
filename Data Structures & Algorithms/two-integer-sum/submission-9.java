bc75602172c15520a660e4dae3844fe3708dc251
ESudarshan <sudarshanekambe@gmail.com>
2026-06-08 17:13:29 +0530

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n= nums.length;
+
+        int[][] numsi = new int[n][2];
+        for(int i=0; i<n; i++) {
+            numsi[i][0] = nums[i];
+            numsi[i][1] = i;
+        }
+
+        Arrays.sort(numsi, (a, b) -> a[0] - b[0]);
+
+        int l=0;
+        int r=n-1;
+        while(l < r) {
+            int sum = numsi[l][0] + numsi[r][0];
+            if(sum == target) {
+                return new int[] {Math.min(numsi[l][1], numsi[r][1]), Math.max(numsi[l][1], numsi[r][1])};
+            } else if(sum < target) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return new int[0];
+    }
+}

becaf8fcd7240a3841669d29c2a4fa2149e5302f
ESudarshan <sudarshanekambe@gmail.com>
2026-06-08 16:23:37 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s == null || t == null) {
+            return false;
+        }
+
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> sMap = new HashMap();
+        Map<Character, Integer> tMap = new HashMap();
+        for(int i=0; i<s.length(); i++) {
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        if(sMap.size() != tMap.size()) {
+            return false;
+        }
+
+        for(char key : sMap.keySet()) {
+            if(!tMap.containsKey(key) || !sMap.get(key).equals(tMap.get(key))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

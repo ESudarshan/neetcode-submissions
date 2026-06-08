9e7f7316ea5f60bc7a9bf837e1c1ff839d228f1d
ESudarshan <sudarshanekambe@gmail.com>
2026-06-08 16:31:45 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s == null || t == null) {
+            return false;
+        }
+
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        int[] map = new int[26];
+        for(int i=0; i<s.length(); i++) {
+            map[s.charAt(i)-'a']++;
+            map[t.charAt(i)-'a']--;
+        }
+
+        for(int i=0; i<26; i++) {
+            if(map[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

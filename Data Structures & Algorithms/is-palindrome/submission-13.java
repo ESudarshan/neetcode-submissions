2d5bb7d07617d9cbde12ac8f6e7867811a52cfc4
ESudarshan <sudarshanekambe@gmail.com>
2026-06-10 14:54:45 +0530

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int n = s.length();
+        StringBuilder sb =  new StringBuilder("");
+        for(int i=0; i<n; i++) {
+            if(isAlphaNumeric(s, i)) {
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
+        }
+        String og = sb.toString();
+        String reversed = sb.reverse().toString();
+        return og.equals(reversed);
+    }
+
+    boolean isAlphaNumeric(String s, int i) {
+        if((s.charAt(i) >= 'a' && s.charAt(i) <= 'z')
+            || (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z')
+            || (s.charAt(i) >= '0' && s.charAt(i) <= '9')) {
+                return true;
+            }
+        return false;
+    }
+}

0ec25ce0da0b54756e231659ed8b62c032cc4404
ESudarshan <sudarshanekambe@gmail.com>
2026-06-13 21:34:21 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+ 
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++) {
+            int d=0;
+            for(int j=i+1; j<n; j++) {
+                if(temperatures[j] > temperatures[i]) {
+                    d = j-i;
+                    break;
+                }
+            }
+            ans[i] = d;
+        }
+        return ans;
+
+    }
+}

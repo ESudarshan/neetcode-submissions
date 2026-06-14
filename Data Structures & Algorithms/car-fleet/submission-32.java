6b2240c65f7ff11b4f947ea34b3badcaf23c083a
ESudarshan <sudarshanekambe@gmail.com>
2026-06-14 14:35:49 +0530

    Add: car-fleet - submission-32

---
+++ Data Structures & Algorithms/car-fleet/submission-32.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        double[] track = new double[target];
+        for(int i=0; i<n; i++) {
+            track[position[i]] = (double)(target-position[i])/speed[i];
+        }
+
+        double prevTime = 0.0;
+        int fleets = 0;
+        for(int i=target-1; i>=0; i--) {
+            if(track[i] > 0.0) {
+                if(track[i] > prevTime) {
+                    prevTime = track[i];
+                    fleets++;
+                }
+            }
+        }
+        return fleets;
+    }
+}

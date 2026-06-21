a586524cd5f38e946edb007d52f88748b6c01215
ESudarshan <sudarshanekambe@gmail.com>
2026-06-21 21:56:41 +0530

    Add: serialize-and-deserialize-binary-tree - submission-10

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-10.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        dfsi(root, sb);
+        return sb.toString();
+    }
+
+    public void dfsi(TreeNode root, StringBuilder sb) {
+        if(root == null) {
+            sb.append("N,");
+            return;
+        }
+        sb.append(root.val);
+        sb.append(",");
+        dfsi(root.left, sb);
+        dfsi(root.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        int[] i = new int[1];
+        return dfso(data, i);
+    }
+
+    public TreeNode dfso(String data, int[] i) {
+        if(i[0] == data.length()) {
+            return null;
+        }
+        int comma = data.indexOf(",", i[0]);
+        String token = data.substring(i[0], comma);
+        i[0] = comma + 1;
+        if(token.equals("N")) {
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(token));
+        node.left = dfso(data, i);
+        node.right = dfso(data, i);
+        return node;
+    }
+
+}

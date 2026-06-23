01c7db80b88a3e06f1284266a130fbccaa7387d6
ESudarshan <sudarshanekambe@gmail.com>
2026-06-24 01:37:12 +0530

    Add: task-scheduling - submission-23

---
+++ Data Structures & Algorithms/task-scheduling/submission-23.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char ch : tasks) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for(char ch : map.keySet()) {
+            maxHeap.offer(map.get(ch));
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+        int c = 0;
+        while(!maxHeap.isEmpty() || !q.isEmpty()) {
+            c++;
+            if(maxHeap.isEmpty()) {
+                c = q.peek()[0]; 
+            } else if(!maxHeap.isEmpty()) {
+                int f = maxHeap.poll();
+                f--;
+                if(f != 0) {
+                    q.add(new int[] { c + n, f});
+                }
+            }
+
+            if(!q.isEmpty() && q.peek()[0] == c) {
+                int[] e = q.remove();
+                maxHeap.offer(e[1]);
+            }
+        }
+
+        return c;
+    }
+}

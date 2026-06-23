c30beb443db379bb6325cf85ea956e83c099892f
ESudarshan <sudarshanekambe@gmail.com>
2026-06-23 21:49:22 +0530

    Add: design-twitter-feed - submission-35

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-35.java
@@ -0,0 +1,56 @@
+class Twitter {
+
+    Map<Integer, Set<Integer>> following;
+    Map<Integer, List<int[]>> tweets;
+    int timestamp;
+
+    public Twitter() {
+        following = new HashMap<>();
+        tweets = new HashMap<>();        
+        timestamp = 0;
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        tweets.putIfAbsent(userId, new ArrayList<>());
+        tweets.get(userId).add(new int[] {timestamp++, tweetId});
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        Set<Integer> users = new HashSet<>();
+        users.add(userId);
+        if(following.containsKey(userId)) {
+            users.addAll(following.get(userId));
+        }
+
+        List<int[]> tweetList = new ArrayList<>();
+        for(int followeeId : users) {
+            if(tweets.containsKey(followeeId)) {
+                for(int[] tweet : tweets.get(followeeId)) {
+                    tweetList.add(tweet);
+                }
+            }    
+        } 
+        
+        Collections.sort(tweetList, (a, b) -> b[0] - a[0]);
+
+        List<Integer> tweetIds = new ArrayList<>();
+        for(int i=0; i<tweetList.size(); i++) {
+            tweetIds.add(tweetList.get(i)[1]);
+            if(tweetIds.size() == 10) {
+                break;
+            }
+        }
+        return tweetIds;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        following.putIfAbsent(followerId, new HashSet<>());
+        following.get(followerId).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        if(following.containsKey(followerId)) {
+            following.get(followerId).remove(followeeId);
+        }
+    }
+}

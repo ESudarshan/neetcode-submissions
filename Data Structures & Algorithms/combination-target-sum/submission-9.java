2a54b5513864b9e22127dcc6cecf4281892539e5
ESudarshan <sudarshanekambe@gmail.com>
2026-06-24 22:38:40 +0530

    Add: combination-target-sum - submission-9

---
+++ Data Structures & Algorithms/combination-target-sum/submission-9.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> output = new ArrayList<>();
+        Arrays.sort(nums);
+        combinationSum(nums, 0, target, new ArrayList<>(), output);
+        return output;
+    }
+
+    public void combinationSum(int[] nums, int i, int target, List<Integer> combo, List<List<Integer>> output) {
+        if(target == 0) {
+            output.add(new ArrayList<>(combo));
+            return;
+        }
+
+        if(i == nums.length || target < 0) {
+            return;
+        }
+        
+        if(target - nums[i] < 0) {
+            return;
+        }
+
+        combo.add(nums[i]);
+        combinationSum(nums, i, target - nums[i], combo, output);
+        combo.remove(combo.size() - 1);
+        combinationSum(nums, i+1, target, combo, output);
+    }
+}

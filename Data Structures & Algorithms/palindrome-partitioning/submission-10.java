3b749a629b6cb7369a0a5bfa2b443b38b2e3ca29
ESudarshan <sudarshanekambe@gmail.com>
2026-06-26 00:00:29 +0530

    Add: palindrome-partitioning - submission-10

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-10.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> output = new ArrayList<>();
+        backtrack(s, 0, 0, new ArrayList<>(), output);
+        return output;
+    }
+
+    public void backtrack(String s, int i, int k, List<String> l, List<List<String>> output) {
+        if(k == s.length()) {
+            if(i == k) {
+                output.add(new ArrayList<>(l));
+                 return;
+            }
+            return;
+        }
+
+        if(isStringPalindrome(s, i, k)) {
+            l.add(s.substring(i, k + 1));  
+            backtrack(s, k + 1, k + 1, l, output);
+            l.remove(l.size() - 1);
+        }
+
+        backtrack(s, i, k + 1, l, output);
+    }
+
+    public boolean isStringPalindrome(String s, int l, int r) {
+        while(l < r) {
+            if(s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}

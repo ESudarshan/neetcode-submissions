01c7287432420e323a80bfd1e4df8423aaadc687
ESudarshan <sudarshanekambe@gmail.com>
2026-06-29 01:03:14 +0530

    Add: partition-labels - submission-7

---
+++ Data Structures & Algorithms/partition-labels/submission-7.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> res = new ArrayList<>();
+        int n = s.length();
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            map.put(s.charAt(i), i);
+        }
+        
+        int i=0;
+        while(i < n) {
+            int start = i;
+            int end = i;
+            while(start <= end) {
+                end = Math.max(end, map.get(s.charAt(start)));
+                start++;
+            }
+            res.add(end - i + 1);
+            i = start;
+        }
+        return res;
+    }
+}

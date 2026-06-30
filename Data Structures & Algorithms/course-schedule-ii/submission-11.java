d698c8da28d0fa3966bf7ab9d9175d0cc952fc2f
ESudarshan <sudarshanekambe@gmail.com>
2026-07-01 01:30:23 +0530

    Add: course-schedule-ii - submission-11

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-11.java
@@ -0,0 +1,50 @@
+class Solution {
+    
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    Set<Integer> inProgress = new HashSet<>();
+    Set<Integer> completed = new HashSet<>();
+    List<Integer> output = new ArrayList<>();
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int n = prerequisites.length;
+
+        for(int i=0; i<n; i++) {
+            map.computeIfAbsent(prerequisites[i][0], k -> new ArrayList<>()).add(prerequisites[i][1]);
+        }
+        for(int i=0; i<numCourses; i++) {
+            if(!isFinished(i)) {
+                return new int[0];
+            }
+        }
+
+        int[] ans = new int[numCourses];
+        int i = 0;
+        for(int c : output) {
+            ans[i++] = c;
+        }
+        return ans;
+    }
+
+    public boolean isFinished(int course) {
+        if(completed.contains(course)) {
+            return true;
+        }
+        if(inProgress.contains(course)) {
+            return false;
+        }
+        inProgress.add(course);
+        List<Integer> dependencies = map.getOrDefault(course, new ArrayList<>());
+        for(int dependency : dependencies) {
+            if(!isFinished(dependency)) {
+                return false;
+            }
+        }
+        inProgress.remove(course);
+        completed.add(course);
+        map.remove(course);
+        output.add(course);
+        return true;
+    }
+
+}
+

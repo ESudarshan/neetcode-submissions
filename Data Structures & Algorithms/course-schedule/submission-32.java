58728e8fa0cbc96adc28d9c4c3e237184a95d3e1
ESudarshan <sudarshanekambe@gmail.com>
2026-07-01 02:05:17 +0530

    Add: course-schedule - submission-32

---
+++ Data Structures & Algorithms/course-schedule/submission-32.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    Set<Integer> inProgress = new HashSet<>();
+    Set<Integer> completed = new HashSet<>();
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int n = prerequisites.length;
+        for(int i=0; i<n; i++) {
+            map.computeIfAbsent(prerequisites[i][0], k -> new ArrayList<>()).add(prerequisites[i][1]);
+        }
+        for(int i=0; i<numCourses; i++) {
+            if(!isFinished(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isFinished(int course) {
+        if(completed.contains(course)) {
+            return true;
+        }
+        if(inProgress.contains(course)) {
+            return false;
+        }
+        inProgress.add(course);
+        List<Integer> dependencies = map.getOrDefault(course, new ArrayList<>());
+        for(int dependency : dependencies) {
+            if(!isFinished(dependency)) {
+                return false;
+            }
+        }
+        inProgress.remove(course);
+        completed.add(course);
+        return true;
+    }
+
+}

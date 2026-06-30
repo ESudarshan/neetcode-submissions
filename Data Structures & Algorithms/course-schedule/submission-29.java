5e4972f7d66a0b3b87f2c8ae548753a5766d9b6a
ESudarshan <sudarshanekambe@gmail.com>
2026-07-01 01:49:17 +0530

    Add: course-schedule - submission-29

---
+++ Data Structures & Algorithms/course-schedule/submission-29.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    Map<Integer, List<Integer>> map = new HashMap<>();
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indeg = new int[numCourses];
+        for(int[] prereq : prerequisites) {
+            map.computeIfAbsent(prereq[1], x -> new ArrayList<>()).add(prereq[0]);
+            indeg[prereq[0]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<numCourses; i++) {
+            if(indeg[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        int completed = 0;
+        while(!q.isEmpty()) {
+            int course = q.poll();
+            completed++;
+            for(int dependent : map.getOrDefault(course, new ArrayList<>())) {
+                indeg[dependent]--;
+                if(indeg[dependent] == 0) {
+                    q.add(dependent);
+                }
+            }
+        }
+
+        return numCourses == completed;
+    }
+}

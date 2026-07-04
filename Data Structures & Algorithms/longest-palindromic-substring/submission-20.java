b030c89a48a441147b43dc1419fd89c16dbe1ab2
ESudarshan <sudarshanekambe@gmail.com>
2026-07-04 22:06:59 +0530

    Add: longest-palindromic-substring - submission-20

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-20.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    int start = 0;
+    int maxLen = 0;
+    Boolean[][] mem;
+
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        mem = new Boolean[n][n];
+        pal(s, 0, n-1);
+        return s.substring(start, start + maxLen);
+    }
+
+    public boolean pal(String s, int i, int j) {
+        if(i > j) {
+            return false;
+        }
+
+        if(mem[i][j] != null) {
+            return mem[i][j];
+        }
+
+        boolean isPal = false;
+        if(s.charAt(i) == s.charAt(j)
+            && (j-i+1 <= 3 || pal(s, i+1, j-1))) {
+            isPal = true;
+        }
+
+        if(isPal && j-i+1 > maxLen) {
+            maxLen = j-i+1;
+            start = i; 
+        }
+
+        mem[i][j] = isPal;
+
+        pal(s, i+1, j);
+        pal(s, i, j-1);
+        
+        return isPal;
+    }
+}

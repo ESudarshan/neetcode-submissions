83a45613b9a979c326a3c9382c11943e5aaf9260
ESudarshan <sudarshanekambe@gmail.com>
2026-07-08 00:48:30 +0530

    Add: longest-common-subsequence - submission-30

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-30.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[] dp = new int[n+1];
+        int diagonal;
+        for(int i=m-1; i>=0; i--) {
+            diagonal = dp[n];
+            for(int j=n-1; j>=0; j--) {
+                int temp = dp[j];
+                if(text1.charAt(i) == text2.charAt(j)) {
+                    dp[j] = 1 + diagonal;
+                } else {
+                    dp[j]= Math.max(dp[j], dp[j+1]);
+                }            
+                diagonal = temp;
+            }    
+        }
+
+        return dp[0];
+    }
+
+}

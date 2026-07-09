a3d229927e9be73027885a5dc14145f8aa4395f3
ESudarshan <sudarshanekambe@gmail.com>
2026-07-09 15:25:36 +0530

    Add: target-sum - submission-45

---
+++ Data Structures & Algorithms/target-sum/submission-45.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    int mem[][];
+    
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+        int sum = 0;
+        for(int num : nums) {
+            sum += num;
+        }
+        mem = new int[n][sum*2+1];
+        return sum(nums, 0, n, target);    
+    }
+
+    public int sum(int[] nums, int i, int n, int target) {
+        if(i == n) {
+            if(target == 0) {
+                return 1;
+            }
+            return 0;
+        }
+
+        return sum(nums, i+1, n, target + nums[i]) + sum(nums, i+1, n, target - nums[i]);
+    }
+
+}
